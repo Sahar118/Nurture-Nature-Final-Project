b74da689fe65c59aaf090703db9008491983812d
sahar118 <sahar.t118@gmail.com>
2023-04-15 18:51:34 +0300

    message Schema done

--- server/models/dist/messageModel.dev.js
+++ server/models/dist/messageModel.dev.js
@@ -4,6 +4,22 @@ var mongoose = require('mongoose');
 
 var messageSchema = new mongoose.Schema({
   chat: {
-    type: mongoose.Schema.Types.ObjectId
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "chats"
+  },
+  sender: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "users"
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  read: {
+    type: Boolean,
+    required: false
   }
+}, {
+  timestamps: true
 });
+module.exports = mongoose.model('messages', messageSchema);
--- server/models/messageModel.js
+++ server/models/messageModel.js
@@ -3,6 +3,23 @@ const mongoose = require('mongoose');
 const messageSchema = new mongoose.Schema({
     chat: {
         type: mongoose.Schema.Types.ObjectId,
+        ref: "chats"
+    },
+    sender: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "users"
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    read: {
+        type: Boolean,
+        required: false,
+    },
+},
+    {
+        timestamps: true,
+    })
 
-    }
-})
+module.exports = mongoose.model('messages', messageSchema);
